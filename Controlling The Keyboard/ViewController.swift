5bd83bfaac083d4b3dae4e6fe451ed2b9a61a4f6
Calvin Cheng <linchuan.cheng@gmail.com>
2015-03-13 22:18:51 +0800

    Controlling the keyboard

--- Controlling The Keyboard/ViewController.swift
+++ Controlling The Keyboard/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var entry: UITextField!
+    
+    @IBAction func submit(sender: AnyObject) {
+        
+        self.result.text = entry.text
+        if entry.text.isEmpty {
+            
+            println("no value provided")
+            self.result.text = "Please provide a value"
+        }
+        
+    }
+
+    @IBOutlet var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.entry.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
 
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        textField.resignFirstResponder()
+    
+        return true
+    }
 
 }
 
